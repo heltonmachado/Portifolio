e5d7e7186441ebc021651a7774a33f372b722ac6
Helton Machado <89802002+heltonmachado@users.noreply.github.com>
2024-05-19 16:28:25 -0300

    upload
    upload repositório

---
+++ autoplay.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const audioPlayer = document.getElementById("audioPlayer");
+    audioPlayer.volume = 1.0; // Define o volume (0.0 a 1.0)
+});
